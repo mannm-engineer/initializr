b3a2a3f7d87080488eac21c271edb93ae2bb7e12
tech-conqueror <mannm.engineer@gmail.com>
2025-06-06 16:37:45 +0700

    initial commit

---
+++ src/test/java/com/sudo/initializr/InitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudo.initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
